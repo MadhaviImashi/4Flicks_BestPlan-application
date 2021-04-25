87378e94432a3197eb4e6230b10452e2ac6388e3
Miru <madhaviimashi@gmail.com>
2021-04-25 11:18:44 +0530

    BMI activity designed

--- app/src/main/java/com/example/a4flicksapp/bmiCalculator.java
+++ app/src/main/java/com/example/a4flicksapp/bmiCalculator.java
@@ -3,6 +3,11 @@ package com.example.a4flicksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class bmiCalculator extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class bmiCalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+
+        //radioGroup = findViewById(R.id.radioGroup);
+        //Button buttonApply = findViewById(R.id.button_apply);
+
+        //display the customized Tost message at the loading of this activity
+        //creating the layoutInflater instance
+        LayoutInflater li = getLayoutInflater();
+        //getting the view object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.custom_toast, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+
+        //creating the Toast object
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0,0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
     }
 }
