a2f1da3a11416ba1e02bf90823029a25849189fd
Miru <madhaviimashi@gmail.com>
2021-04-25 11:44:53 +0530

    BMI activity linked with HomePage

--- app/src/main/java/com/example/a4flicksapp/MainActivity.java
+++ app/src/main/java/com/example/a4flicksapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.a4flicksapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNavBMI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNavBMI = findViewById(R.id.btnNavBMI);
+    }
+
+    public void displayBMIactivity(View view){
+
+        Intent intent = new Intent(this, bmiCalculator.class);
+
+        startActivity(intent);
     }
 }
