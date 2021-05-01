14c1b7dd7433f14d555c14da8dc27ff635937a13
HeliSahanya <heli.sahanya22@gmail.com>
2021-05-01 23:39:39 +0530

    Design GroceryListAddItem Activity

---
+++ app/src/main/java/com/example/a4flicksapp/GroceryListAddItem.java
@@ -0,0 +1,17 @@
+package com.example.a4flicksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GroceryListAddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery_list_add_item);
+    }
+
+}
