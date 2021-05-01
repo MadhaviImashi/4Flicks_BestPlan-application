cd758ddc69ca8d6347825718625dad24e25ed5c8
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-01 19:23:46 +0530

    linked update button on recipe activity to update activity

--- app/src/main/java/com/example/a4flicksapp/recipe.java
+++ app/src/main/java/com/example/a4flicksapp/recipe.java
@@ -3,6 +3,9 @@ package com.example.a4flicksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class recipe extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class recipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+    }
+
+    public void displayUpdateActivity(View view){
+        Intent intent = new Intent(this, update_recipe.class);
+        startActivity(intent);
     }
 }
