c404ab9381a0da1d117543e144d6eed6de08ac20
HeliSahanya <heli.sahanya22@gmail.com>
2021-05-02 00:43:33 +0530

    Created GroceryListItemList Activity using recyclerview

---
+++ app/src/main/java/com/example/a4flicksapp/GroceryListItemList.java
@@ -0,0 +1,14 @@
+package com.example.a4flicksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroceryListItemList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery_list_item_list);
+    }
+}
