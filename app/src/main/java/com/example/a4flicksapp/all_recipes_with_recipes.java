414deba11b528a7a92031f1a8b39f1ea92d86273
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-01 18:08:02 +0530

    linked add button on all recipes activity to create recipes.class

--- app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
+++ app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
@@ -3,6 +3,9 @@ package com.example.a4flicksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class all_recipes_with_recipes extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class all_recipes_with_recipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_recipes_with_recipes);
+    }
+
+    public void displayAllRecipesWithRecipes(View view){
+        Intent intent = new Intent(this, create_recipe.class);
+        startActivity(intent);
     }
 }
