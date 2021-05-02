f9618607955a9bff3304b714b69bbdfaa053db94
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-02 14:56:34 +0530

    Defined variables in activity_all_recipes_with_recipes.java class

--- app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
+++ app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class all_recipes_with_recipes extends AppCompatActivity {
+
+    Button addRecipeBtn;
+    ListView recipeList;
+    TextView noOfRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
