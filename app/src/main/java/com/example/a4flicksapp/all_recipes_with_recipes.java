aa1c71fe5873f3ea71dbabd347d6d4b0b149f364
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-02 16:57:18 +0530

    Added following to the all_recipes_with_recipes.java file AddRecipe = findViewById(R.id.addRecipeBtn); RecipeList = findViewById(R.id.recipeList); NoOfRecipes = findViewById(R.id.noOfRecipes);

--- app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
+++ app/src/main/java/com/example/a4flicksapp/all_recipes_with_recipes.java
@@ -1,28 +1,36 @@
 package com.example.a4flicksapp;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class all_recipes_with_recipes extends AppCompatActivity {
 
-    Button addRecipeBtn;
-    ListView recipeList;
-    TextView noOfRecipes;
+    private ImageButton AddRecipe;
+    private ListView RecipeList;
+    private TextView NoOfRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_recipes_with_recipes);
+
+        AddRecipe = findViewById(R.id.addRecipeBtn);
+        RecipeList = findViewById(R.id.recipeList);
+        NoOfRecipes = findViewById(R.id.noOfRecipes);
     }
 
     public void displayAllRecipesWithRecipes(View view){
         Intent intent = new Intent(this, create_recipe.class);
         startActivity(intent);
     }
+
+
 }
