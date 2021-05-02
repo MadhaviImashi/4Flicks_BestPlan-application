8dac7d09400d158326c83f583cf659c2d0a29d23
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-02 17:30:40 +0530

    Modified recipe.java file with variable declarations and their findViewById methods.

--- app/src/main/java/com/example/a4flicksapp/recipe.java
+++ app/src/main/java/com/example/a4flicksapp/recipe.java
@@ -6,13 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class recipe extends AppCompatActivity {
+
+    private TextView NameOfRecipe;
+    private ImageView ImageOfRecipe;
+    private TextView IngredientsOfRecipe;
+    private TextView StepsOfRecipe;
+    private Button UpdateRecipeBtn;
+    private Button DeleteRecipeBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        NameOfRecipe = findViewById(R.id.RecipeName);
+        ImageOfRecipe = findViewById(R.id.RecipeImage);
+        IngredientsOfRecipe = findViewById(R.id.ingredientsList);
+        StepsOfRecipe = findViewById(R.id.Steps);
+        UpdateRecipeBtn = findViewById(R.id.Updatebtn);
+        DeleteRecipeBtn = findViewById(R.id.Deletebtn);
     }
 
     public void displayUpdateActivity(View view){
