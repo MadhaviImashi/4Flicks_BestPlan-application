e472222c5bb5d1bd93795d705c7df75b7cb8a960
Miru <madhaviimashi@gmail.com>
2021-05-02 15:28:57 +0530

    nav method for TasksList actvity created

--- app/src/main/java/com/example/a4flicksapp/FragmentedTips.java
+++ app/src/main/java/com/example/a4flicksapp/FragmentedTips.java
@@ -3,13 +3,14 @@ package com.example.a4flicksapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class FragmentedTips extends AppCompatActivity {
 
-    Button btnUnderW, btnNormal, btnOverW; //create button objects for fragment buttons
+    Button btnNavDailyPlan, btnUnderW, btnNormal, btnOverW; //create button objects for fragment buttons
     Fragment frgUnderW, frgNormal, frgOverW; //fragment objects
 
     @Override
@@ -20,11 +21,21 @@ public class FragmentedTips extends AppCompatActivity {
         btnUnderW = findViewById(R.id.btnUnderW);
         btnNormal = findViewById(R.id.btnNormal);
         btnOverW = findViewById(R.id.btnOverW);
+        btnNavDailyPlan = findViewById(R.id.btnNavTasksList);
 
         //instantiate objects for each fragment.java class
         frgUnderW = new underweightFragment();
         frgNormal = new normalFragment();
         frgOverW = new overweightFragment();
+
+        //set onclick listener to link the btn with the next actvity
+        btnNavDailyPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayTasksList(v);
+            }
+        });
+
     }
 
     @Override
@@ -51,5 +62,10 @@ public class FragmentedTips extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.frgBody, frgOverW).commit();
             }
         });
+    }
+
+    public void displayTasksList(View view){
+        Intent intent = new Intent(this, ToDoList.class);
+        startActivity(intent);
     }
 }
