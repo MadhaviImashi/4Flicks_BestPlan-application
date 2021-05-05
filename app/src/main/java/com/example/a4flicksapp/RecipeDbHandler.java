6a74e3a7121d0cf34b80ec7e2c4dba4d9e44e02d
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-05 12:50:36 +0530

    Created methods to connect with database, create database table for recipe, drop table recipe in RecipeDbHandler

--- app/src/main/java/com/example/a4flicksapp/RecipeDbHandler.java
+++ app/src/main/java/com/example/a4flicksapp/RecipeDbHandler.java
@@ -11,18 +11,48 @@ public class RecipeDbHandler extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DB_NAME = "BestPlanDatabase";
     private static final String TABLE_NAME = "recipe";
+
+
+    //Column Names
+    private static final String ID  = "id";
+    private static final String RECIPE_NAME  = "Recipe_Name";
+    private static final String RECIPE_INGREDIENTS  = "Recipe_Ingredients";
+    private static final String RECIPE_DIRECTIONS  = "Recipe_Directions";
+    private static final String STARTED  = "started";
+    private static final String FINISHED  = "finished";
+
+
     public RecipeDbHandler(@Nullable Context context) {
         super(context, DB_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        /* CREATE TABLE recipe(id INTEGER PRIMARY KEY AUTOINCREMENT, Recipe_Name TEXT, Recipe_Ingredients TEXT, Recipe_Directions TEXT, Recipe_Image ) */
+
+        String TABLE_CREATE_QUERY = "CREATE TABLE "+TABLE_NAME+" "+
+                "("
+                +ID+"INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +RECIPE_NAME+" TEXT,"
+                +RECIPE_INGREDIENTS+" TEXT,"
+                +RECIPE_DIRECTIONS+" TEXT,"
+                +STARTED+" TEXT,"
+                +FINISHED+" TEXT" +
+                ");";
+        /* CREATE TABLE recipe(id INTEGER PRIMARY KEY AUTOINCREMENT, Recipe_Name TEXT, Recipe_Ingredients TEXT, Recipe_Directions TEXT) */
+
+        db.execSQL(TABLE_CREATE_QUERY);
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        //Drops the older table if exists
+        db.execSQL(DROP_TABLE_QUERY);
+        //Creates the table again
+        onCreate(db);
 
     }
 }
