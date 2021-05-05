bd06be950f080ec07185df0b7f5e5da3aee0eed2
Charmie98 <56944762+Charmie98@users.noreply.github.com>
2021-05-05 12:38:53 +0530

    Removed variables related to recipe image from update recipe.java

--- app/src/main/java/com/example/a4flicksapp/update_recipe.java
+++ app/src/main/java/com/example/a4flicksapp/update_recipe.java
@@ -13,8 +13,6 @@ public class update_recipe extends AppCompatActivity {
     private TextInputEditText EditRecipeName;
     private TextInputEditText EditRecipeIngredients;
     private TextInputEditText EditDirections;
-    private Button ImageEdit;
-    private ImageView EditRecipeInputImage;
     private Button RecipeUpdate;
 
     @Override
@@ -25,8 +23,6 @@ public class update_recipe extends AppCompatActivity {
         EditRecipeName = findViewById(R.id.editRecipeName);
         EditRecipeIngredients = findViewById(R.id.editRecipeIngredients);
         EditDirections = findViewById(R.id.editDirections);
-        ImageEdit = findViewById(R.id.imageEdit);
-        EditRecipeInputImage = findViewById(R.id.editRecipeInputImage);
         RecipeUpdate = findViewById(R.id.recipeUpdate);
     }
 }
