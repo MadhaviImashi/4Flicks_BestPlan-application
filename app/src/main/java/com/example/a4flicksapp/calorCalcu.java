82b5d748d803d55918c4650e80e50dd1349d29d0
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-06 16:26:14 +0530

    validations added for calorie calculator.

--- app/src/main/java/com/example/a4flicksapp/calorCalcu.java
+++ app/src/main/java/com/example/a4flicksapp/calorCalcu.java
@@ -3,6 +3,7 @@ package com.example.a4flicksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
@@ -24,33 +25,45 @@ public class calorCalcu extends AppCompatActivity {
 
     }
 
-    public void calorieCalcu(View view){
-        String br1=calbr.getText().toString();
-        String lu1=callu.getText().toString();
-        String din1=caldin.getText().toString();
+    public void calorieCalcu(View view) {
+        String br1 = calbr.getText().toString();
+        String lu1 = callu.getText().toString();
+        String din1 = caldin.getText().toString();
+        if (TextUtils.isEmpty(calbr.getText())) {
+            calbr.setError("Enter valid input");
 
-        float calvalues=Float.parseFloat(br1);
-        float calvalues2=Float.parseFloat(lu1);
-        float calvalues3=Float.parseFloat(din1);
-
-        float adds= addcal(calvalues,calvalues2,calvalues3);
-        float calintake=adds;
-
-
-
-        if(calintake<2000){
-            calresult="Your Calorie Intake Should Be Higher";
-
-        }else if (calintake>2700){
-            calresult="Your Calorie Intake Should Be Lower";
-
-        }else if (calintake>=2000 && calintake<=2700){
-            calresult="Your Calorie Intake Is Perfect";
         }
-        calcalculation="Calorie Intake \n" +calintake+ "\n"+calresult;
-        calres.setText(calcalculation);
+        if (TextUtils.isEmpty(callu.getText())) {
+            callu.setError("Enter valid input");
+
+        }
+        if (TextUtils.isEmpty(caldin.getText())) {
+            caldin.setError("Enter valid input");
+            caldin.requestFocus();
+        } else {
+
+            float calvalues = Float.parseFloat(br1);
+            float calvalues2 = Float.parseFloat(lu1);
+            float calvalues3 = Float.parseFloat(din1);
+
+            float adds = addcal(calvalues, calvalues2, calvalues3);
+            float calintake = adds;
 
 
+            if (calintake < 2000) {
+                calresult = "Your Calorie Intake Should Be Higher";
+
+            } else if (calintake > 2700) {
+                calresult = "Your Calorie Intake Should Be Lower";
+
+            } else if (calintake >= 2000 && calintake <= 2700) {
+                calresult = "Your Calorie Intake Is Perfect";
+            }
+            calcalculation = "Calorie Intake \n" + calintake + "\n" + calresult;
+            calres.setText(calcalculation);
+
+
+        }
     }
 
     protected float addcal(float x1,float x2,float x3){ return  x1+x2+x3;}
