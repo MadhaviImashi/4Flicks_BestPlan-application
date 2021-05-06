962c260ab3d3ce7d4678e13c055a5cfe6da82bef
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-06 15:50:47 +0530

    edittext field validations added for update meal and add meal UIs.

--- app/src/main/java/com/example/a4flicksapp/AddMeal.java
+++ app/src/main/java/com/example/a4flicksapp/AddMeal.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,18 +33,36 @@ public class AddMeal extends AppCompatActivity {
         add1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userday1=day1.getText().toString();
-                String userbreakfirst1=breakfirst1.getText().toString();
-                String userlunch1=lunch1.getText().toString();
-                String userdinner1=dinner1.getText().toString();
-                long started = System.currentTimeMillis();
+                if (TextUtils.isEmpty(day1.getText())) {
+                    day1.setError("Enter Day");
+                    day1.requestFocus();
+                }
+                if (TextUtils.isEmpty(breakfirst1.getText())) {
+                        breakfirst1.setError("Enter Breakfast");
+                        breakfirst1.requestFocus();
+                    }
+                if (TextUtils.isEmpty(lunch1.getText())) {
+                    lunch1.setError("Enter Lunch");
+                    lunch1.requestFocus();
+                }
+                if (TextUtils.isEmpty(dinner1.getText())) {
+                    dinner1.setError("Enter Dinner");
+                    dinner1.requestFocus();
+                }
+                else {
+                    String userday1 = day1.getText().toString();
+                    String userbreakfirst1 = breakfirst1.getText().toString();
+                    String userlunch1 = lunch1.getText().toString();
+                    String userdinner1 = dinner1.getText().toString();
+                    long started = System.currentTimeMillis();
 
-                mealPlan meals1= new mealPlan(userday1,userbreakfirst1,userlunch1,userdinner1,started,0);
-                dbHandler.addMealplan(meals1);
-                Toast.makeText(AddMeal.this, "Meal Plan Added", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context,listOfMeals.class));
+                    mealPlan meals1 = new mealPlan(userday1, userbreakfirst1, userlunch1, userdinner1, started, 0);
+                    dbHandler.addMealplan(meals1);
+                    Toast.makeText(AddMeal.this, "Meal Plan Added", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(context, listOfMeals.class));
 
 
+                }
             }
         });
     }
--- app/src/main/java/com/example/a4flicksapp/editMeal.java
+++ app/src/main/java/com/example/a4flicksapp/editMeal.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,18 +41,37 @@ public class editMeal extends AppCompatActivity {
         editm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usday=day3.getText().toString();
-                String usbr=break3.getText().toString();
-                String uslu=lunch3.getText().toString();
-                String usdin=dinner3.getText().toString();
-                updateDate1=System.currentTimeMillis();
 
-                mealPlan meal8= new mealPlan(Integer.parseInt(id1),usday,usbr,uslu,usdin,updateDate1,0);
-                int state = dbHandler.updateSingleMeal(meal8);
-                System.out.println(state);
-                Toast.makeText(editMeal.this, "Meal Plan Updated", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context,listOfMeals.class));
+                if (TextUtils.isEmpty(day3.getText())) {
+                    day3.setError("Enter Day");
+                    day3.requestFocus();
+                }
+                else if (TextUtils.isEmpty(break3.getText())) {
+                    break3.setError("Enter Breakfast");
+                    break3.requestFocus();
+                }
+               else if (TextUtils.isEmpty(lunch3.getText())) {
+                    lunch3.setError("Enter Lunch");
+                    lunch3.requestFocus();
+                }
+               else if (TextUtils.isEmpty(dinner3.getText())) {
+                    dinner3.setError("Enter Dinner");
+                    dinner3.requestFocus();
+                }
+                else {
+                    String usday = day3.getText().toString();
+                    String usbr = break3.getText().toString();
+                    String uslu = lunch3.getText().toString();
+                    String usdin = dinner3.getText().toString();
+                    updateDate1 = System.currentTimeMillis();
 
+                    mealPlan meal8 = new mealPlan(Integer.parseInt(id1), usday, usbr, uslu, usdin, updateDate1, 0);
+                    int state = dbHandler.updateSingleMeal(meal8);
+                    System.out.println(state);
+                    Toast.makeText(editMeal.this, "Meal Plan Updated", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(context, listOfMeals.class));
+
+                }
             }
         });
 
